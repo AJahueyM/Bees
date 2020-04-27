5aa2eb03a5d2ff1d6bda275087c56b202a681526
Alberto Jahuey Moncada <albertojahueymoncada@gmail.com>
2020-04-27 10:33:16 -0500

    Bees

---
+++ bee.py
@@ -0,0 +1,17 @@
+import keyboard
+import time
+import re
+
+f = open("bee.txt", "r")
+
+bee_lines = f.readlines()
+
+time.sleep(3)
+
+for line in bee_lines:
+	words = line.split()
+	for word in words:
+		#print(word)
+		keyboard.write(" {}".format(word.lower()), delay=0.05)
+		keyboard.press_and_release("enter")
+		time.sleep(0.3)
