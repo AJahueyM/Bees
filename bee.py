4b47c44ed57bdf3fd7eb3c99045f25ceaa073c32
Alberto Jahuey Moncada <albertojahueymoncada@gmail.com>
2020-04-27 11:36:23 -0500

    Speed

--- bee.py
+++ bee.py
@@ -12,6 +12,6 @@ for line in bee_lines:
 	words = line.split()
 	for word in words:
 		#print(word)
-		keyboard.write(" {}".format(word.lower()), delay=0.05)
+		keyboard.write(" {}".format(word.lower()), delay=0.01)
 		keyboard.press_and_release("enter")
-		time.sleep(0.3)
+		time.sleep(0.1)
